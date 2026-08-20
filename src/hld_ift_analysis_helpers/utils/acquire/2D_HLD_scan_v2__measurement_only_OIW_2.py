b53586ad01ed7f67f56c095ce75727286e04f55d
Pim Dankloff <pim.dankloff@ru.nl>
2026-08-20 13:49:14 +0200

    2d_hld_scan_v2_measurment_only_oiw_2.py update

--- src/hld_ift_analysis_helpers/utils/acquire/2D_HLD_scan_v2__measurement_only_OIW_2.py
+++ src/hld_ift_analysis_helpers/utils/acquire/2D_HLD_scan_v2__measurement_only_OIW_2.py
@@ -341,7 +341,7 @@ for i in range(n):
 
     print(f'@address: {meas_loc.toDict()}\n content: {op.well_by_address(meas_loc).solution.toDict()}')
    
-    #ift_measurement.measure(loc_to_measure, f"conc_{i/(n-1):.05f}", "")
+    ift_measurement.measure(loc_to_measure, f"conc_{i/(n-1):.05f}", "")
 
 
 op.drop_tip_at_origin(a_pipette, intent)
